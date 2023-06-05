caf74ed4bb9f912044c51d50aad68e0248e39c85
Olivia Zheng <99567408+Oli1021@users.noreply.github.com>
2023-06-06 05:01:51 +1000

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template,redirect,url_for
+from .forms import SearchForm
 
 bp = Blueprint('main', __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    form = SearchForm()
+    if form.validate_on_submit():
+        event_name = form.event_name.data
+        result = search_by_event_name(event_name)
+        if result:
+            return redirect(url_for('main.event_detail', event_name=event_name))
+    return render_template('index.html', form=form)
