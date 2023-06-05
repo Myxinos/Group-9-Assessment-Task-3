5d8ae425cae7ce3474a3ece2f3b98a2306c9bfe8
Frances Sun <397696202ss@gmail.com>
2023-06-05 23:34:26 +1000

    database

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,6 @@ db=SQLAlchemy()
 #create a function that creates a web application
 # a web server will run this web application
 def create_app():
-  
     app=Flask(__name__)  # this is the name of the module/package that is calling this app
     app.debug=True
     app.secret_key='somesecretgoeshere'
