b77dd416f382383f5a44a1293cff485226e9f36a
Olivia Zheng <99567408+Oli1021@users.noreply.github.com>
2023-06-06 10:33:24 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,2 +1,9 @@
 from flask_login import UserMixin
 from . import db
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+def search_event_by_keyword(keyword):
+    return db.session.query(Event).filter(Event.name.contains(keyword)).first()
