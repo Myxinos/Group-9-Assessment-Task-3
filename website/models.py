5c82a1cfe6b8882b7008569d226aa78eca487296
Olivia Zheng <99567408+Oli1021@users.noreply.github.com>
2023-06-06 11:51:34 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -2,7 +2,12 @@ from . import db
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    address= db.Column(db.String(256), nullable=False)
+    status = db.Column(db.String(64), nullable=False)
+    category= db.Column(db.String(64), index=True, nullable=False)
+    image = db.Column(db.String(60), nullable=False, default='default.jpg')
 
 def search_event_by_keyword(keyword):
     return db.session.query(Event).filter(Event.name.contains(keyword)).first()
